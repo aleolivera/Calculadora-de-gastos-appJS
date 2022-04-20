0308c85fa12b20f116702e8d827addbe01cafd00
aleolivera <62443592+aleolivera@users.noreply.github.com>
2022-04-20 03:33:48 -0300

    carga de tabla secundaria

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-const agregarGasto = (gastos) => {
+
+const agregarGasto = () => {
+    let listaGastos;
     const amigo= new String(document.getElementById("amigo").value);
     const item = new String(document.getElementById("item").value);
     const cantidad = new String(document.getElementById("cantidad").value);
@@ -9,13 +11,46 @@ const agregarGasto = (gastos) => {
         cantidad:cantidad,
         importe:importe,
     }
-    if(gastos!=null){
-        gastos.add(gasto);
+    if(sessionStorage.getItem("listaGastos")){
+        listaGastos=JSON.parse(sessionStorage.getItem("listaGastos"));
     }
     else{
-        gastos = new Array(gasto);
+        listaGastos= new Array();
     }
-    alert("amigo: " + gasto.amigo + ", item: " + gasto.item +
-     ", cantidad: " + gasto.cantidad + ", importe: $" + gasto.importe);
+    listaGastos.push(gasto);
+    /*
+    listaGastos.forEach(g => {
+        document.getElementById("prueba").innerHTML+= "<h3>"+g.item+"</h3>";
+        console.log("amigo: " + g.amigo + ", item: " + g.item + ", cantidad: " 
+                        + g.cantidad + ", importe: $" + g.importe);
+    });
+    */
+    sessionStorage.setItem("listaGastos",JSON.stringify(listaGastos));
+
+    /*
+    console.log("amigo: " + gasto.amigo + ", item: " + gasto.item +
+    ", cantidad: " + gasto.cantidad + ", importe: $" + gasto.importe + ", session: "+sessionStorage.getItem("gasto"));
+    //alert("amigo: " + gasto.amigo + ", item: " + gasto.item +
+    //", cantidad: " + gasto.cantidad + ", importe: $" + gasto.importe + ", session: "+sessionStorage.getItem("gasto"));
+    /*
+    sessionStorage.setItem('user', JSON.stringify(user));
+    var obj = JSON.parse(sessionStorage.user);
+    */
+}
+
+const cargarGastos =()=>{
+    console.log("cargarGastos");
+    let listaGastos;
+    if(sessionStorage.getItem("listaGastos")){
+        listaGastos=JSON.parse(sessionStorage.getItem("listaGastos"));
+        listaGastos.forEach(g => {
+            document.getElementById("prueba").innerHTML+= "<h3>"+g.item+"</h3>";
+            console.log("amigo: " + g.amigo + ", item: " + g.item + ", cantidad: " 
+                            + g.cantidad + ", importe: $" + g.importe);
+        });
+    }
+}
+const eliminarGasto = ()=>{
+    alert("eliminarGasto!");
 }
 
